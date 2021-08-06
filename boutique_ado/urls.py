22739fb61caee4278d0dbb499d0679fa6c1bf1f1
bubsy07 <bubsy_07@HOTMAIL.CO.UK>
2021-08-06 12:08:38 +0000

    added products view

--- boutique_ado/urls.py
+++ boutique_ado/urls.py
@@ -1,3 +1,4 @@
+  
 """boutique_ado URL Configuration
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
@@ -21,4 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
+    path('products/', include('products.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
